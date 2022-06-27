b848586e36b177eeff49fb5727a2ed978df36cde
Zelim <anzorov.zelim@gmail.com>
2022-06-27 18:13:51 +0300

    form validation

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+  const [textDirty, setTextDirty] = useState(false);
+  const [textError, setTextError] = useState(
+    "Поле ввода не должно быть пустым"
+  );
+  const [message, setMessage] = useState("");
+  const [sent, setSent] = useState(false);
+
+  const handleBlur = () => {
+    setTextDirty(true);
+  };
+
+  const handleText = (e) => {
+    setText(e.target.value);
+    if (!e.target.value) {
+      setTextError("Поле ввода не должно быть пустым!");
+    } else {
+      setTextError("");
+    }
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(text);
+    setSent(true);
+    setMessage("Сообщение успешно отправлено");
+  };
+
+  return (
+    <div className="app">
+      <h2>Форма подписки</h2>
+      <form>
+        <input
+          onChange={handleText}
+          value={text}
+          onBlur={handleBlur}
+          type="text"
+          className={`${textDirty && textError ? "is-error" : ""}`}
+        />
+        <button disabled={text ? false : true} onClick={handleClick}>
+          Отправить
+        </button>
+      </form>
+      {textDirty && textError && <div style={{ color: "red" }}>{textError}</div>}
+      {setSent && !textError && <div style={{ color: "green" }}>{message}</div>}
+    </div>
+  );
+}
+
+export default App;
